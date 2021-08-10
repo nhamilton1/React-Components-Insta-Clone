4a571f67c2044ae68f6721a4c9c49898ac21ea86
nhamilton1 <nickh3592@gmail.com>
2021-08-10 14:02:51 -0400

    finished comments

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
       <Posts likePost={likePost} posts={posts}/>
+
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -21,9 +21,14 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection 
+      // likePost={() => likePost(post.id)} 
+      likePost={likePost}
+      numbOfLikes={post.likes}
+      key={post.id}
+      />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/>
     </div>
   );
 };
