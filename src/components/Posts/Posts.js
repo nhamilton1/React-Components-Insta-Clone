f221e242c56d71f8eb5df4045c9aa63d7e57247b
nhamilton1 <nickh3592@gmail.com>
2021-08-10 11:49:47 -0400

    i think im understanding, working on comments now

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Post from './Post';
 import './Posts.css';
-import App from '../../App';
+
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
@@ -10,6 +10,15 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(postObjs => {
+        return (
+          <Post
+          key={postObjs.id}
+          post={postObjs}
+          likePost={likePost}
+          />
+        )
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
